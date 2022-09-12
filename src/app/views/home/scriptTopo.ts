0133df475842f26fc5fbc068244aa49249f34a3d
NicolasLacerda <nicolas.lacerda.work@gmail.com>
2022-09-12 17:06:18 -0300

    .

--- src/app/views/home/scriptTopo.ts
+++ src/app/views/home/scriptTopo.ts
@@ -25,10 +25,21 @@ const scriptTopo = () => {
     $('.loginPosition').animate({ height: 'toggle' }, 300);
   });
 
-  $('.caixaDePesquisa').on('click', function () {
-    $('.dropdownPesquisa').animate({ height: 'toggle' }, 'fast');
-    $(':input').css('border-bottom-left-radius', '0px');
-    $('.icon3').css('border-bottom-right-radius', '0px');
+  $('.caixaDePesquisa').click(function () {
+    var clicks = $(this).data('clicks');
+    if (clicks) {
+      $('.caixaDePesquisaPosition').removeClass('active');
+      $('.dropdownPesquisa').css('display', 'none');
+      $(':input').css('border-bottom-left-radius', '5px');
+      $('.icon3').css('border-bottom-right-radius', '5px');
+    } else {
+      $('.caixaDePesquisaPosition').addClass('active');
+      $('.dropdownPesquisa').css('display', 'flex');
+      $(':input').css('border-bottom-left-radius', '0px');
+      $('.icon3').css('border-bottom-right-radius', '0px');
+    }
+
+    $(this).data('clicks', !clicks);
   });
 };
 
